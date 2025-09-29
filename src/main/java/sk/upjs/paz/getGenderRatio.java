ca5e8203bd66781242def9b63b51023a2df18b80
mmark <5410498@upjs.sk>
2025-09-29 13:18:52 +0200

    gerderRatio

--- src/main/java/sk/upjs/paz/getGenderRatio.java
+++ src/main/java/sk/upjs/paz/getGenderRatio.java
@@ -3,7 +3,7 @@ package sk.upjs.paz;
 import lombok.With;
 
 @With
-public record PomerGenderov(int male, int female, int mutant) {
+public record getGenderRatio(int male, int female, int mutant) {
 
     public double pomerChlapcovKuDievcatam() {
         return (double) male / (double) (female + male + mutant);
