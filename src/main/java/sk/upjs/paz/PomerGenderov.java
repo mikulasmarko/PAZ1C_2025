e38f62a24f849302bf75908b9a9fa9c7aba515db
mmark <5410498@upjs.sk>
2025-09-29 13:08:52 +0200

    check

--- src/main/java/sk/upjs/paz/PomerGenderov.java
+++ src/main/java/sk/upjs/paz/PomerGenderov.java
@@ -8,10 +8,8 @@ public record PomerGenderov(int male, int female, int mutant) {
     public double pomerChlapcovKuDievcatam() {
         return (double) male / (double) (female + male + mutant);
     }
-
     public double pomerDievcatKuChlapcom() {
         return (double) female / (double) (female + male + mutant);
     }
-
 
 }
